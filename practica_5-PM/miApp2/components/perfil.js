c2f20a46a3bef8cd8bc372767065620881ba3dd0
Isaac Sánchez <123047038@upq.edu.mx>
2026-06-16 20:11:33 -0600

    Practica No.8: StyleSheet

--- practica_5-PM/miApp2/components/perfil.js
+++ practica_5-PM/miApp2/components/perfil.js
@@ -1,18 +1,66 @@
-import { View, StyleSheet, Text } from "react-native";
+/* Perfil usando destructuración */
+import { View, Text, Button, StyleSheet } from 'react-native';
+import React,{useState} from 'react';
 
-export const Perfil = () => {
+export const Perfil = ({ nombre, carrera, materia, cuatrimestre, estiloExterno }) => {
+
+    const [mostrar, setMostrar] = useState(false);
+
     return(
-        <View style={styles.container}>
-            <Text>Nombre: Sánchez López Isaac Abdiel</Text>
-            <Text>Carrera: Ingeniería en Sistemas Computacionales</Text>
-            <Text>Materia: Programación Móvil</Text>
-            <Text>Cuatrimestre: 9°</Text>
+        <View style = {[styles.tarjeta, estiloExterno]}>
+            <Text style = {styles.nombre}>{nombre}</Text>
+        {mostrar && 
+        <>
+            <Text style = {styles.carrera}>Carrera: {carrera}</Text>
+            <Text style = {styles.otroTexto}>Materia: {materia}</Text>
+            <Text style = {styles.otroTexto}>Cuatrimestre: {cuatrimestre}</Text>
+        </>
+        }
+
+        <Button title= "Mostrar perfil"
+        onPress={() => setMostrar(!mostrar)}
+        />
+
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
+    nombre: {
+        fontSize: 24,
+        fontWeight: 700,
+        textTransform: 'uppercase',
     },
+    carrera: {
+        fontSize: 18,
+        color: 'blue',
+        fontFamily: 'Roboto',
+    },
+    otroTexto: {
+        fontSize: 12,
+        fontFamily: 'Courier',
+        fontStyle: 'italic',
+    },
+    tarjeta: {
+        borderWidth: 4,
+        margin: 20,
+        padding: 25,
+    }
 });
+
+/* 
+Perfil usando Props
+
+import { View, Text } from 'react-native';
+
+export const Perfil = (props) => {
+    return (
+        <View>
+            <Text>{props.nombre}</Text>
+            <Text>{props.carrera}</Text>
+            <Text>{props.materia}</Text>
+            <Text>{props.cuatrimestre}</Text>
+        </View>
+    );
+}
+*/
